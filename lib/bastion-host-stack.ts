66abd2d729cbf1ed26217caf149b0174ef85f7c3
ktoyod <ytdh7834@gmail.com>
2022-06-21 21:03:13 +0900

    bastion hostの実装

--- lib/bastion-host-stack.ts
+++ lib/bastion-host-stack.ts
@@ -1,16 +1,31 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, aws_ec2 as ec2, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BastionHostStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'vpc', {
+      cidr: '10.0.0.0/16',
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'isolated',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BastionHostQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const host = new ec2.BastionHostLinux(this, 'bastion-host', {
+      vpc,
+      subnetSelection: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+    });
+
+    new CfnOutput(this, 'start-session-command', {
+      value: `aws ssm start-session --target ${host.instanceId}`,
+    });
   }
 }
